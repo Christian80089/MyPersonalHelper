6fcd440c15172e5d452da45c3f97e5e939f7372d
chris <christiandelprete01@gmail.com>
2025-12-30 22:57:28 +0100

    added themeToggle

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,12 @@
 import { LoginForm } from '@/components/login-form'
 
+import { ThemeToggle } from '@/components/theme-toggle'
+
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
+        <ThemeToggle />
         <LoginForm />
       </div>
     </div>

a4c239e7049116541d7fe4f393d8c274e561837d
chris <christiandelprete01@gmail.com>
2026-01-03 15:16:01 +0100

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,12 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import { Outfit } from 'next/font/google';
+import './globals.css';
+import "flatpickr/dist/flatpickr.css";
+import { SidebarProvider } from '@/context/SidebarContext';
+import { ThemeProvider } from '@/context/ThemeContext';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const outfit = Outfit({
   subsets: ["latin"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "My Personal Helper",
-  description: "My Personal Helper is a DB manager tool",
-};
 
 export default function RootLayout({
   children,
@@ -24,17 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
+    <html lang="en">
+      <body className={`${outfit.className} dark:bg-gray-900`}>
+        <ThemeProvider>
+          <SidebarProvider>{children}</SidebarProvider>
         </ThemeProvider>
       </body>
     </html>
--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/page.tsx - Landing page leggera
-import { redirect } from 'next/navigation'
-
-export default function Home() {
-  redirect('/auth/login')
-}
